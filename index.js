c12c83d4540f7619521e27fa9ecd08c77ba5db16
Yudhajit Chowdhury <110385709+berryBatKnave@users.noreply.github.com>
2023-08-12 13:56:05 +0530

    NewsLetter SignUp Page v0.2
    Completed the success page.
    
    Implemented the passing of the email from one page to another with the help of local-storage for javascript.
    
    localStorage.setItem(mailStorage, mail);
    
    -Storaing the value first from the passed value of mail to a local variable declared "mailStorage".
    
    var value = localStorage.getItem(mailStorage);
    $(".container p b").text(value);
    
    -Then with the help of jQuery changed the innerText for the bold element and replaced with the local stored value.
    
    
    NextSteps : Webpage needs a phone version / version for smaller devices that is yet to be implemented.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+var mailStorage;
+
 function ValidateEmail(mail) 
 {
  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
   {
     console.log("True");
+    // $(".container p b").html(mail);
+    localStorage.setItem(mailStorage, mail);
     window.location.href = "./success.html";
     // $(".container").hide().css("visibility", "hidden");
   }
@@ -10,4 +14,9 @@ function ValidateEmail(mail)
     $(".news-letter span h3").css("color","hsl(4, 100%, 67%)");
     console.log("False");
   }
+}
+
+function changeEmail(){
+  var value = localStorage.getItem(mailStorage);
+  $(".container p b").text(value);
 }
